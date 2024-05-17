a954c6b9d2c30adccf5853751d1678b5d431022f
sakiishii <167153141+sakiishii@users.noreply.github.com>
2024-05-17 18:18:09 +0900

    Add files via upload

--- js/shu.js
+++ js/shu.js
@@ -1,13 +1,22 @@
-$(function(){
-    // 1回目のアクセス
-    if($.cookie("access") == undefined) {
-      //最初にアクセスしたときにはここに書いたアニメーションのJSが動く
-      $.cookie("access","onece");
-      $("#shutter").css("display","block")
-    // 2回目以降は動かないようにするけど最初は動かす
-    } else {
-  $("#shutter").css("display","none")
-      // 2回目以降は動かないようにする
-    }
-  });
+// $(function(){
+//     // 1回目のアクセス
+//     if($.cookie("access") == undefined) {
+//       //最初にアクセスしたときにはここに書いたアニメーションのJSが動く
+//       $.cookie("access","onece");
+//       $("#shutter").css("display","block")
+//     // 2回目以降は動かないようにするけど最初は動かす
+//     } else {
+//   $("#shutter").css("display","none")
+//       // 2回目以降は動かないようにする
+//     }
+//   });
+    // JavaScriptでドロップダウンを制御する関数
+    function toggleDropdown() {
+      var dropdown = document.getElementById("myDropdown");
+      if (dropdown.style.display === "none" || dropdown.style.display === "") {
+          dropdown.style.display = "block";
+      } else {
+          dropdown.style.display = "none";
+      }
+  }
   
